9f40a361789b5b6e9985fdd44ba131b6c137d328
Ramia1205 <ramiaambreen@gmail.com>
2024-04-10 14:51:58 -0400

    Ramia A

---
+++ raa_Lab9.py
@@ -0,0 +1,48 @@
+def encode(password):
+    encoded = ""
+    for element in password:
+        if element == "0":
+            encoded += "3"
+        elif element == "1":
+            encoded += "4"
+        elif element == "2":
+            encoded += "5"
+        elif element == "3":
+            encoded += "6"
+        elif element == "4":
+            encoded += "7"
+        elif element == "5":
+            encoded += "8"
+        elif element == "6":
+            encoded += "9"
+        elif element == "7":
+            encoded += "0"
+        elif element == "8":
+            encoded += "1"
+        elif element == "9":
+            encoded += "2"
+    return (encoded)
+
+def decode():
+    return None
+def main():
+
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+
+        if option == 2:
+            decode()
+
+        if option == 3:
+            break
+
+
+if __name__ == "__main__":
+    main()
